53f2da613b1e812ddb5862d405dfc881d43262b7
unknown <rhrabin934602@gmail.com>
2022-04-04 08:46:57 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import { Route, Routes } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import Cart from './components/Cart/Cart';
-import Grandpa from './components/Grandpa/Grandpa';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import NotFound from './components/NotFound/NotFound';
-import Order from './components/Order/Order';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/home' element={<Home></Home>}></Route>
-        <Route path='/order' element={<Order></Order>}></Route>
-        <Route path='/cart' element={<Cart></Cart>}></Route>
-        <Route path='/grandpa' element={<Grandpa></Grandpa>}></Route>
-
-        <Route path='*' element={<NotFound></NotFound>}></Route>
-      </Routes>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
